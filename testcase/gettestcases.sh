90145d66106b0f0497e3666cd182240fcf7b5a0b
hecor <husthecor@gmail.com>
2013-03-26 00:14:43 +0800

    delete sh

--- testcase/gettestcases.sh
+++
@@ -1,6 +0,0 @@
-for i in {0..99}
-do
-	wget https://trade.gtja.com/webtrade/commons/verifyCodeImage.jsp?ran=0.5582681659143418
-	mv verifyCodeImage.jsp\?ran\=0.5582681659143418 testsets/test$i.jpg
-done
-
